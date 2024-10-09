ee1d9e22cd6c23af47000e76ceb87cba9980fa7f
YKDman <83913024+ykdman@users.noreply.github.com>
2024-10-10 03:18:20 +0900

    Update swagger-initializer.js

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/ykdman/test-swagger-ui/refs/heads/master/swagger.json",
+    url: "https://raw.githubusercontent.com/ykdman/test-be/refs/heads/gh-pages/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
